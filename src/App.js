973581217f40bfbad4abe02f2fefc4fb28fa3a0d
Ahmed_Shtya <Shtya54@gmail.com>
2024-01-25 20:43:18 +0200

    5

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
 
   return ( 
     <div className='App'>
-      {/* <UP  /> */}
+      <UP  />
       <Navbar />
       <Routes>
         <Route  path='/'    element={<Home />} />                           
