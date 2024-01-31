72101eff50fd146245a85fbd3decff1d0336e459
Ahmed_Shtya <Shtya54@gmail.com>
2024-01-31 11:40:32 +0200

    7

--- src/components/Hero2.jsx
+++ src/components/Hero2.jsx
@@ -1,32 +1,35 @@
 import React from 'react'
-import Img1 from "../assets/bg/10.webp"
+import Img1 from "../assets/bg/5.webp"
+import Img2 from "../assets/bg/12.webp"
+import Img3 from "../assets/bg/9.webp"
+import Img4 from "../assets/bg/8.webp"
 
 
 const Hero2 = () => {
 
     const itmes = [
         {path:"/" ,img:Img1 , h1:"Ahmed" , h2:"Design" , h3:"Animal" , desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maxime praesentium rerum molestias! Alias ratione, repellat vero doloremque aut repudiandae ducimus incidunt tempora illum, rerum, odio repellendus sed. Nulla, sapiente!"  },
-        {path:"/" ,img:Img1 , h1:"Ahmed" , h2:"Design" , h3:"Animal" , desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maxime praesentium rerum molestias! Alias ratione, repellat vero doloremque aut repudiandae ducimus incidunt tempora illum, rerum, odio repellendus sed. Nulla, sapiente!"  },
-        {path:"/" ,img:Img1 , h1:"Ahmed" , h2:"Design" , h3:"Animal" , desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maxime praesentium rerum molestias! Alias ratione, repellat vero doloremque aut repudiandae ducimus incidunt tempora illum, rerum, odio repellendus sed. Nulla, sapiente!"  },
-        {path:"/" ,img:Img1 , h1:"Ahmed" , h2:"Design" , h3:"Animal" , desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maxime praesentium rerum molestias! Alias ratione, repellat vero doloremque aut repudiandae ducimus incidunt tempora illum, rerum, odio repellendus sed. Nulla, sapiente!"  },
+        {path:"/" ,img:Img2 , h1:"Ahmed" , h2:"Design" , h3:"Animal" , desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maxime praesentium rerum molestias! Alias ratione, repellat vero doloremque aut repudiandae ducimus incidunt tempora illum, rerum, odio repellendus sed. Nulla, sapiente!"  },
+        {path:"/" ,img:Img3 , h1:"Ahmed" , h2:"Design" , h3:"Animal" , desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maxime praesentium rerum molestias! Alias ratione, repellat vero doloremque aut repudiandae ducimus incidunt tempora illum, rerum, odio repellendus sed. Nulla, sapiente!"  },
+        {path:"/" ,img:Img4 , h1:"Ahmed" , h2:"Design" , h3:"Animal" , desc:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi maxime praesentium rerum molestias! Alias ratione, repellat vero doloremque aut repudiandae ducimus incidunt tempora illum, rerum, odio repellendus sed. Nulla, sapiente!"  },
     ]
 
     const thumbnail = [
         {img:Img1 , h1 :"title" , h2:"description"},
-        {img:Img1 , h1 :"title" , h2:"description"},
-        {img:Img1 , h1 :"title" , h2:"description"},
-        {img:Img1 , h1 :"title" , h2:"description"},
+        {img:Img2 , h1 :"title" , h2:"description"},
+        {img:Img3 , h1 :"title" , h2:"description"},
+        {img:Img4 , h1 :"title" , h2:"description"},
     ]
   return (
     <div className='Hero2'> 
-        <div className="carousel container">
+        <div className="carousel container next">
             <div className="list">
                 {
                     itmes.map((e,index)=>(
                         <div className="item" key={index}>
                             <img src={e.img} alt="" />
                             <div className="content">
-                                <div className="autho"> {e.h1} </div>
+                                <div className="author"> {e.h1} </div>
                                 <div className="title"> {e.h2} </div>
                                 <div className="topic"> {e.h3} </div>
                                 <div className="des"> {e.desc} </div>
@@ -54,6 +57,11 @@ const Hero2 = () => {
                     ))
                 }
                 
+            </div>
+
+            <div className="arrows">
+                <div id="prev"> {`<`} </div>
+                <div id="next"> {`>`} </div>
             </div>
         </div>
     </div>
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -8,8 +8,11 @@ import P_Imgmain1 from  "../assets/portfolio/2.jpg"
 import P_Imgmain2 from  "../assets/portfolio/3.jpg"
 import P_Imgmain3 from  "../assets/portfolio/4.jpg"
 import P_Imgmain4 from  "../assets/portfolio/5.jpg"
+import P_Imgmain5 from  "../assets/portfolio/intro.jpg"
+import P_Imgmain6 from  "../assets/portfolio/1.jpg"
 
 import { Link } from 'react-router-dom'
+import Divider from '../components/Divider';
 
 
 const Projects = () => {
@@ -18,21 +21,17 @@ const Projects = () => {
   const [Next , Setnext] =  useState({from:0 , to:4})
 
   const [projects , setProjects] = useState([
-    {  curr: 0 , id: 1 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-    {  curr: 0 , id: 2 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-    {  curr: 0 , id: 3 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-    {  curr: 0 , id: 4 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
+    {  curr: 0 , id: 1 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4 , P_Imgmain5 , P_Imgmain6]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
+    {  curr: 0 , id: 2 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4 , P_Imgmain5 , P_Imgmain6]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
+    {  curr: 0 , id: 3 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4 , P_Imgmain5 , P_Imgmain6]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
+    {  curr: 0 , id: 4 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4 , P_Imgmain5 , P_Imgmain6]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
     
-    {  curr: 0 , id: 5 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-    {  curr: 0 , id: 6 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-    {  curr: 0 , id: 7 ,  images:{main:P_Imgmain2 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-    {  curr: 0 , id: 8 ,  images:{main:P_Imgmain2 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
+    {  curr: 0 , id: 5 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4 , P_Imgmain5 , P_Imgmain6]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
+    {  curr: 0 , id: 6 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4 , P_Imgmain5 , P_Imgmain6]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
+    {  curr: 0 , id: 7 ,  images:{main:P_Imgmain2 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4 , P_Imgmain5 , P_Imgmain6]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
+    {  curr: 0 , id: 8 ,  images:{main:P_Imgmain2 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4 , P_Imgmain5 , P_Imgmain6]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
     
-    {  curr: 0 , id: 9 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-    {  curr: 0 , id: 10 ,  images:{main:P_ImgCover1 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-    {  curr: 0 , id: 11 ,  images:{main:P_Imgmain3 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-    {  curr: 0 , id: 12 ,  images:{main:P_Imgmain3 , submain:[P_Imgmain1 ,P_Imgmain2 ,P_Imgmain3 ,P_Imgmain4]} , routes:"" , title:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة" , desc:"تطوير مشاريع الأعمال يتطلب رؤية واضحة واستراتيجية محكمة، يجب على رواد الأعمال فهم احتياجات السوق وتحليل المنافسة، الابتكار والتسويق الذكي يمكن أن يسهما في نجاح المشاريع، توظيف التكنولوجيا وبناء فريق عمل متميز يعدان أساسيين للنمو المستدام." },
-  ])
+    ])
 
   const handleImg = (IdxOfImg , id , IdxOfPro)=>{
   setindexImg(IdxOfImg)
@@ -51,8 +50,10 @@ const Projects = () => {
   return (
     <div className='portfolio' >
       <div className="coverIntro "> <img src={ImgIntro} alt="intro page contains word 'about us '" /></div>
-      
+        <Divider />
       <div className="project">
+      <div className="h1-head">OUR PORTFOLIO</div>
+
         <div className="container">
             {
               projects.slice(Next.from , Next.to)?.map((e,index)=>(
@@ -75,16 +76,7 @@ const Projects = () => {
             }
         </div>
 
-        {/* <div className="pagination">
-          <button   onClick={_=> Pagination({from:0 , to:4})}  className={`btn-nav left-btn `} >   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="left-icon">     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />   </svg> </button>
-          <div className="page-numbers">
-            <button  onClick={_=> Pagination({from:0 , to:4})}  className={`btn-page ${Next.from == 0 ? "btn-selected" : ""} `}    > 1</button>
-            <button  onClick={_=> Pagination({from:4 , to:8})}  className={`btn-page ${Next.from == 4 ? "btn-selected" : ""} `}    > 2</button>
-            <button  onClick={_=> Pagination({from:8 , to:12})}  className={`btn-page ${Next.from == 8 ? "btn-selected" : ""} `}    > 3</button>
-          </div>
-          <button onClick={_=>  Pagination({from:8 , to:12}) }  className={`btn-nav right-btn `}  > <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="right-icon"> <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" /> </svg> </button>
-        </div> */}
-
+        {Next.to == 8 ? "" : <div className="bt" onClick={_=> Setnext({from:0 , to : 8})}>see more</div>}
       </div>
       
 </div>
